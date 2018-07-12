460293d93cbc425c282bb9933a4983aa2c723033
Kurt Collins <kurt@kurt.sx>
2018-07-12 19:11:01 +0200

    Changing the default assetRoot.

--- gulpfile.js
+++ gulpfile.js
@@ -270,7 +270,7 @@ function generateBootScript(manifest) {
   var defaultAssetRoot;
 
   if (process.env.NODE_ENV === 'production') {
-    defaultAssetRoot = `https://travrse-js.bitballoon.com/`; 
+    defaultAssetRoot = `https://cdn.travr.se/`; 
     // defaultAssetRoot = `https://cdn.hypothes.is/hypothesis/${version}/`;
   } else {
     defaultAssetRoot = `http://${packageServerHostname()}:3001/hypothesis/${version}/`;
