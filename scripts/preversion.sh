543151d9edb8ece53a6b0853b18d3a5f89b31663
Chapman Snowden <Chapman.Snowden@gmail.com>
2018-07-16 16:02:36 -0700

    point versioning script towards signal io github

--- scripts/preversion.sh
+++ scripts/preversion.sh
@@ -7,7 +7,7 @@ git tag --sign --message "Dummy Tag" dummy-tag
 git tag --delete dummy-tag > /dev/null
 
 # Check GitHub API access token
-CLIENT_INFO_URL=https://api.github.com/repos/hypothesis/client
+CLIENT_INFO_URL=https://api.github.com/repos/signal-io/travrse-client
 REPO_TMPFILE=/tmp/client-repo.json
 curl -s -H "Authorization: Bearer $GITHUB_TOKEN" $CLIENT_INFO_URL > $REPO_TMPFILE
 CAN_PUSH=$(node -p -e "perms = require('$REPO_TMPFILE').permissions, perms && perms.push")
